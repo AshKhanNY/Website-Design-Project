eabf1b5fe7e4f80892b3b96ff33cad2b4e17dc79
bryanr1992 <bryan.rivas05@gmail.com>
2022-10-25 16:31:43 -0400

    incorporated and tested semantic-ui CDN for styling

--- anime-list/src/components/pages/Anime.jsx
+++ anime-list/src/components/pages/Anime.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Anime = () => {
-    return (
-        <div>
-            <h3>Anime example</h3>
-        </div>
-    );
-}
+  return (
+    <div>
+        <h3>Anime page dummy</h3>
+    </div>
+  );
+};
 
 export default Anime;
--- anime-list/src/index.js
+++ anime-list/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />, document.querySelector('#root'));
+root.render(<App />);
+
+
+
+//ReactDOM.render(<App />, document.querySelector('#root')); Not supported in react 28
 
