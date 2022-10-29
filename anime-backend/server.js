6c3eeabe75a55d49a8d630e1ddb368970505c00a
bryanr1992 <bryan.rivas05@gmail.com>
2022-10-29 16:32:31 -0400

    initialized sequelize and set up the initial sync function before defining our model

--- anime-backend/server.js
+++ anime-backend/server.js
@@ -16,6 +16,12 @@ app.use(express.json());
 // parse request of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+//We re-sync during development only
+db.sequelize.sync({ force: true}).then(() => {
+    console.log("Drop and re-sync db.");
+})
+
 
 // simple route for testing
 app.get("/", (req, res) => {
