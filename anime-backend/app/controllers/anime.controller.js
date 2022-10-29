3aecb338345de1aee80100dfd1dcb5b9400e1370
bryanr1992 <bryan.rivas05@gmail.com>
2022-10-29 16:58:31 -0400

    set up our initial model with CRUD operations

---
+++ anime-backend/app/controllers/anime.controller.js
@@ -0,0 +1,40 @@
+const db = require("../models");
+const Anime = db.animes;
+const Op = db.Sequelize.Op;
+
+
+// Create and save a new anime
+exports.create = (req, res) =>{
+
+};
+
+// Retrieve all animes from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single anime with an id
+exports.findOne = (req, res) => {
+
+};
+
+// update an anime by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete an anime with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all animes from the database
+exports.deleteAll = (req, res) => {
+
+};
+
+
+// Find all published animes
+exports.findAllPublished = (req, res) => {
+
+};
